81fd7cbab5617ba26e1463330450df81c0c6d3cd
ggsasda <ggsasda@gmail.com>
2024-10-24 19:54:13 +0530

    fix 15

--- loader/src/injector/jni_hooks.hpp
+++ loader/src/injector/jni_hooks.hpp
@@ -275,11 +275,7 @@ void *nativeSpecializeAppProcess_orig = nullptr;
     ctx.nativeSpecializeAppProcess_post();
 }
 [[clang::no_stack_protector]] void nativeSpecializeAppProcess_grapheneos_u(
-    JNIEnv *env, jclass clazz, jint uid, jint gid, jintArray gids, jint runtime_flags,
-    jobjectArray rlimits, jint mount_external, jstring se_info, jstring nice_name,
-    jboolean is_child_zygote, jstring instruction_set, jstring app_data_dir,
-    jboolean is_top_app, jobjectArray pkg_data_info_list, jobjectArray whitelisted_data_info_list,
-    jboolean mount_data_dirs, jboolean mount_storage_dirs, jboolean mount_sysprop_overrides, jlongArray _14
+    JNIEnv *env, jclass clazz, jint uid, jint gid, jintArray gids, jint runtime_flags, jobjectArray rlimits, jint mount_external, jstring se_info, jint _10, jint _11, jstring nice_name, jboolean is_child_zygote, jstring instruction_set, jstring app_data_dir, jboolean is_top_app, jobjectArray pkg_data_info_list, jobjectArray whitelisted_data_info_list, jboolean mount_data_dirs, jboolean mount_storage_dirs, jboolean mount_sysprop_overrides, jlongArray _12
 ) {
     AppSpecializeArgs_v5 args(uid, gid, gids, runtime_flags, rlimits, mount_external, se_info, nice_name, instruction_set, app_data_dir);
     
@@ -295,9 +291,7 @@ void *nativeSpecializeAppProcess_orig = nullptr;
     ctx.nativeSpecializeAppProcess_pre();
 
     reinterpret_cast<decltype(&nativeSpecializeAppProcess_grapheneos_u)>(nativeSpecializeAppProcess_orig)(
-        env, clazz, uid, gid, gids, runtime_flags, rlimits, mount_external, se_info, nice_name,
-        is_child_zygote, instruction_set, app_data_dir, is_top_app, pkg_data_info_list,
-        whitelisted_data_info_list, mount_data_dirs, mount_storage_dirs, mount_sysprop_overrides, _14
+        env, clazz, uid, gid, gids, runtime_flags, rlimits, mount_external, se_info, _10, _11, nice_name, is_child_zygote, instruction_set, app_data_dir, is_top_app, pkg_data_info_list, whitelisted_data_info_list, mount_data_dirs, mount_storage_dirs, mount_sysprop_overrides, _12
     );
 
     ctx.nativeSpecializeAppProcess_post();
@@ -358,8 +352,7 @@ void *nativeForkSystemServer_orig = nullptr;
     return ctx.pid;
 }
 [[clang::no_stack_protector]] jint nativeForkSystemServer_grapheneos_u(
-    JNIEnv *env, jclass clazz, jint uid, jint gid, jintArray gids, jint runtime_flags,
-    jobjectArray rlimits, jlong permitted_capabilities, jlong effective_capabilities
+    JNIEnv *env, jclass clazz, jint uid, jint gid, jintArray gids, jint runtime_flags, jint _13, jint _14, jobjectArray rlimits, jlong permitted_capabilities, jlong effective_capabilities
 ) {
 
     ServerSpecializeArgs_v1 args(uid, gid, gids, runtime_flags, permitted_capabilities, effective_capabilities);
@@ -367,7 +360,7 @@ void *nativeForkSystemServer_orig = nullptr;
     ctx.nativeForkSystemServer_pre();
 
     reinterpret_cast<decltype(&nativeForkSystemServer_grapheneos_u)>(nativeForkSystemServer_orig)(
-        env, clazz, uid, gid, gids, runtime_flags, rlimits, permitted_capabilities, effective_capabilities
+        env, clazz, uid, gid, gids, runtime_flags, _13, _14, rlimits, permitted_capabilities, effective_capabilities
     );
 
     ctx.nativeForkSystemServer_post();
